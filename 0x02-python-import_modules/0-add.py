79674e12b903f4b7e7790e643005e9dfa59ccfc7
shansim <sphinixsnikd@gmail.com>
2022-12-01 04:40:31 -0800

    First task commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-def add(a, b):
-    """My addition function
-    Args:
-        a: first integer
-        b: second integer
-    Returns:
-        The return value. a + b
-    """
-    a
-    b
-    sum = a + b
-    print("{} + {} = {}".format(a, b, sum))
-    return (a + b)
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
